560868f02a716a3a369f02dbc29c159da1824cf2
Michelle Bryson <mbryson@andrew.cmu.edu>
2020-09-11 23:18:59 +0000

    add banner

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //document.body.style.backgroundImage = "linear-gradient(-100deg, lightblue, purple)"
 
+const KEY = "AIzaSyCsJY9a6FXo_lnTP0s7VcU3AKphygCAhEc"
 
-const video = document.getElementById('video');
+const video = document.getElementById('video'),
+      canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d');
 
 function startVideo(){
   navigator.getUserMedia(
@@ -10,33 +13,47 @@ function startVideo(){
     err => console.error(err)
   )
 }
+startVideo();
+
+const url = 'https://vision.googleapis.com/v1/images:annotate?key=' + KEY;
 
 
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-]).then(startVideo);
-
+// Promise.all([
+//   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+//   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+//   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+//   faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+// ]).then(startVideo);
 
 video.addEventListener("playing", () => {
   
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    context.drawImage(video, 0, 0);
+    const img = canvas.toDataURL('image/jpeg', 0.5);
+    
+    
+    
+    const {labelAnnotations} = fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(img)
+    });
 
-    var dominantEmotion = "neutral", dominantVal = 0;
-    if (detections.length != 0){
-      for (var emotion in detections[0]["expressions"]){
-        if (detections[0]["expressions"][emotion] > dominantVal){
-          dominantVal = detections[0]["expressions"][emotion];
-          dominantEmotion = emotion;
-        }
-      }
+    console.log(labelAnnotations)
 
-      console.log(dominantEmotion)
+    // const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
 
-    }
+    // var dominantEmotion = "neutral", dominantVal = 0;
+    // if (detections.length != 0){
+    //   for (var emotion in detections[0]["expressions"]){
+    //     if (detections[0]["expressions"][emotion] > dominantVal){
+    //       dominantVal = detections[0]["expressions"][emotion];
+    //       dominantEmotion = emotion;
+    //     }
+    //   }
+
+    //   console.log(dominantEmotion)
+    //}
     
   }, 1000);
 })
+
