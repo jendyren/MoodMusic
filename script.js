3ba4e2478a07f7d9705486ee66b534f4b1ac205c
cew88 <cew8@rice.edu>
2020-09-11 21:22:39 +0000

    Implement face expression detection with faceapi

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //document.body.style.backgroundImage = "linear-gradient(-100deg, lightblue, purple)"
 
 
-var video = document.getElementById('video');
+const video = document.getElementById('video');
 
 function startVideo(){
   navigator.getUserMedia(
@@ -13,10 +13,30 @@ function startVideo(){
 
 
 Promise.all([
-  //faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
 
 
+video.addEventListener("playing", () => {
+  
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+
+    var dominantEmotion = "neutral", dominantVal = 0;
+    if (detections.length != 0){
+      for (var emotion in detections[0]["expressions"]){
+        if (detections[0]["expressions"][emotion] > dominantVal){
+          dominantVal = detections[0]["expressions"][emotion];
+          dominantEmotion = emotion;
+        }
+      }
+
+      console.log(dominantEmotion)
+
+    }
+    
+  }, 1000);
+})
